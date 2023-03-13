c4608921b89453fecbe1fe6fdac06a6c896e1e47
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-12 21:08:05 -0500

    'this' - Keyword Basics

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
-// nested objects
-// set variable as property value
-// dot notation vs bracket notation
+// this
+// points to the object which is left of the dot
 
-const age = 30;
-
-let random = "random value";
-random = "age";
-
-const person = {
-  name: "john",
-  age: age,
-  siblings: ["anna", "susan", "peter"],
-  greet: function (name) {
-    console.log(`Hi, I am ${name}`);
+const john = {
+  firstName: "john",
+  lastName: "anderson",
+  fullName: function () {
+    console.log(this);
+    console.log(`hello, my name is ${this.firstName} ${this.lastName}`);
   },
-  sayHello(name) {
-    console.log(`Hello, My name is ${name}`);
-  },
-  job: {
-    title: "developer",
-    company: {
-      name: "coding addict",
-      address: "123 main street",
-    },
-  },
-  "random-value": "random",
 };
 
-console.log(person.job.company.name);
+const bob = {
+  firstName: "peter",
+  lastName: "anderson",
+  fullName: function () {
+    console.log(this);
+    console.log(`hello, my name is ${this.firstName} ${this.lastName}`);
+  },
+};
 
-console.log(person.work); // undefined
-
-/* Cannot read properties of undefined (reading 'title') */
-// console.log(person.work.title);
-
-console.log(person["random-value"]);
-
-// check in person object for the property with age which is value of "random"
-console.log(person[random]);
-
-console.log(person);
+john.fullName();
+bob.fullName();
